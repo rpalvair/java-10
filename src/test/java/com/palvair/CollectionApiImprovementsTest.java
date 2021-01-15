0e76194a3994be18950b42863509aff5003d3d3d
ruddy palvair <r.palvair@gmail.com>
2021-01-15 13:57:36 +0100

    tests : Tests on adding element in list instanciated with copyOf() and Collectors.toUnmodifiableList() from a stream

---
+++ src/test/java/com/palvair/CollectionApiImprovementsTest.java
@@ -0,0 +1,30 @@
+package com.palvair;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+import static java.util.List.copyOf;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+public class CollectionApiImprovementsTest {
+
+
+    @Test
+    public void should_throw_exception_when_modifying_copy_of_list() {
+        final List<String> copy = copyOf(Arrays.asList("Hello", "World"));
+
+        assertThatThrownBy(() -> copy.add("")).isInstanceOf(UnsupportedOperationException.class);
+    }
+
+    @Test
+    public void should_throw_exception_when_modifying_unmodifiable_list() {
+        final List<String> copy = Stream.of("Hello", "World")
+                .collect(Collectors.toUnmodifiableList());
+
+        assertThatThrownBy(() -> copy.add("")).isInstanceOf(UnsupportedOperationException.class);
+    }
+}
