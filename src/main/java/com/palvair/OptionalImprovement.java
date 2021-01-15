52fa21cc9a345dedb7c75aa3da937af014e4ffee
ruddy palvair <r.palvair@gmail.com>
2021-01-15 12:01:15 +0100

    tests: Use a test for OptionalImprovement

--- src/main/java/com/palvair/OptionalImprovement.java
+++ src/main/java/com/palvair/OptionalImprovement.java
@@ -1,21 +1,12 @@
 package com.palvair;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-
-import java.util.NoSuchElementException;
 import java.util.Optional;
 
 public class OptionalImprovement {
 
-    private static final Logger LOGGER = LoggerFactory.getLogger(OptionalImprovement.class);
-
-    public static void main(String[] args) {
-        try {
-            Optional.ofNullable(null)
-                    .orElseThrow();
-        } catch (final NoSuchElementException exception) {
-            LOGGER.error(exception.getMessage());
-        }
+    public String callOrElseThrow(final String value) {
+        return Optional.ofNullable(value)
+                .orElseThrow();
     }
+
 }
---
+++ src/test/java/com/palvair/OptionalImprovementTest.java
@@ -0,0 +1,29 @@
+package com.palvair;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.NoSuchElementException;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+public class OptionalImprovementTest {
+
+    @Test
+    public void should_throw_exception_when_optional_is_empty() {
+        final OptionalImprovement optionalImprovement = new OptionalImprovement();
+
+        assertThatThrownBy(() -> optionalImprovement.callOrElseThrow(null)).isInstanceOf(NoSuchElementException.class);
+    }
+
+    @Test
+    public void should_return_value_when_present_in_optional() {
+        final OptionalImprovement optionalImprovement = new OptionalImprovement();
+
+        final String value = optionalImprovement.callOrElseThrow("Hello World");
+
+        assertThat(value).isNotNull().isEqualTo("Hello World");
+    }
+
+
+}
